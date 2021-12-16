fe2cd9e33794fedbec6c55490e158da619bf1b91
Allan Caetano <91976669+allancsilva@users.noreply.github.com>
2021-12-16 13:16:25 -0300

    terminei mappings

--- Luavim/lua/mappings.lua
+++ Luavim/lua/mappings.lua
@@ -1,28 +1,70 @@
--- Sair com 'q' , se usar gravação de macros altere para <C-q> → Ctrl + q
---vim.cmd([[ map q :q<CR> ]])
+--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--
+-- Mapeamentos -----------------------------------------------------------------------------------
+--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--
 
--- Para Salvar com 'Ctrl + S' nos modos: Normal, Inserção e Visual
--- Precisa adicionar a linha: stty -ixon , ao seu ~/.bashrc
+--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--
+-- Sair com 'Ctrl + q'        --------------------------------------------------------------------
+--vim.cmd([[ map q :q<CR> ]]) --------------------------------------------------------------------
+--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--
+vim.cmd([[ nnoremap <C-q> :wq!<CR> ]])
+vim.cmd([[ inoremap <C-q> <Esc>:wq!<CR>l ]])
+vim.cmd([[ vnoremap <C-q> <Esc>:wq!<CR> ]])
+
+
+
+--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--
+-- Para Salvar com 'Ctrl + s' nos modos: Normal, Inserção e Visual -------------------------------
+--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--
 vim.cmd([[ nnoremap <C-s> :w<CR> ]])
 vim.cmd([[ inoremap <C-s> <Esc>:w<CR>l ]])
 vim.cmd([[ vnoremap <C-s> <Esc>:w<CR> ]])
 
+
+
+--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--
+-- Para Telescope 'Ctrl + t' no modo Normal ------------------------------------------------------
+--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--
 vim.cmd([[ nnoremap <C-t> :Telescope<CR>]])
---vim.cmd([[ inoremap <C-a> <Esc>:NERDTreeToggle<CR>l ]])
---vim.cmd([[ vnoremap <C-a> <Esc>:NERDTreeToggle<CR> ]])
 
 
+
+--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--
+-- Para abrir o NvimTreer use 'Ctrl + b' nos modos: Normal, Inserção e Visual --------------------
+--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--
 vim.cmd([[ nnoremap <C-b> :NvimTreeToggle<CR> ]])
 vim.cmd([[ inoremap <C-b> <Esc>:NvimTreeToggle<CR>l ]])
 vim.cmd([[ vnoremap <C-b> <Esc>:NvimTreeToggle<CR> ]])
 
 
 
---vim.cmd([[ nnoremap <C-Tab> :w<CR> ]])
---vim.cmd([[ inoremap <C-Tab> <Esc>:w<CR>l ]])
---vim.cmd([[ vnoremap <C-Tab> <Esc>:w<CR> ]])
+--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--
+-- Selecionar tudo com 'Ctrl + A' ---------------------------------------------------------------- 
+--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--
+vim.cmd([[ map <C-a> ggVG ]])
 
 
+
+--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--
+-- Desfazer tudo com 'Ctrl + Z' ------------------------------------------------------------------
+--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--
+vim.cmd([[ nnoremap <C-z> :u<CR> ]])
+vim.cmd([[ inoremap <C-z> <Esc>:u<CR>l ]])
+vim.cmd([[ vnoremap <C-z> <Esc>:u<CR> ]])
+
+
+
+--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--
+-- Para Circular entre dois buffers com 'Alt + c' nos modos: Normal, Inserção e Visual -----------
+--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--
+vim.cmd([[ nnoremap <A-c> :BufferLineCycleNext<CR> ]])
+vim.cmd([[ inoremap <A-c> <Esc>:BufferLineCycleNext<CR>l ]])
+vim.cmd([[ vnoremap <A-c> <Esc>:BufferLineCycleNext<CR> ]])
+
+
+
+--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--
+-- Para ir direto ao buffers em uma posiçao 'Alt + Posiçao' nos modos: Normal, Inserção e Visual -
+--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--
 vim.cmd([[ nnoremap <A-1> :BufferLineGoToBuffer 1<CR> ]])
 vim.cmd([[ inoremap <A-1> <Esc>:BufferLineGoToBuffer 1<CR>l ]])
 vim.cmd([[ vnoremap <A-1> <Esc>:BufferLineGoToBuffer 1<CR> ]])
@@ -60,52 +102,31 @@ vim.cmd([[ inoremap <A-9> <Esc>:BufferLineGoToBuffer 9<CR>l ]])
 vim.cmd([[ vnoremap <A-9> <Esc>:BufferLineGoToBuffer 9<CR> ]])
 
 
--- " These commands will navigate through buffers in order regardless of which mode you are using
--- " e.g. if you change the order of buffers :bnext and :bprevious will not respect the custom ordering
--- nnoremap <silent>[b :BufferLineGoToBuffer 1<CR>
 
-
--- nnoremap <silent>b] :BufferLineCyclePrev<CR>
-
--- " These commands will move the current buffer backwards or forwards in the bufferline
--- nnoremap <silent><mymap> :BufferLineMoveNext<CR>
--- nnoremap <silent><mymap> :BufferLineMovePrev<CR>
-
--- " These commands will sort buffers by directory, language, or a custom criteria
--- nnoremap <silent>be :BufferLineSortByExtension<CR>
--- nnoremap <silent>bd :BufferLineSortByDirectory<CR>
--- nnoremap <silent><mymap> :lua require'bufferline'.sort_buffers_by(function (buf_a, buf_b) return buf_a.id < buf_b.id end)<CR>
-
-
-
-
-
+--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--
+-- Se não existir o arquivo C++ de extensão .cpp ele preenche da forma abaixo --------------------
+-- altere para sua(s) linguagens preferidas                                   --------------------
+--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--
+vim.cmd([[
+ 	function! AutoCpp()
+ 	  call append(0, '#include <iostream>')
+	  call append(1, '')
+	  call append(2, 'int main( int argc , char **argv ){')
+	  call append(3, "  std::cout << \"Olá, Mundo!\" << '\\n';")
+	  call append(4, '  return 0;')
+	  call append(5, '}')
+	  call cursor(4, 17)
+	endfunction
+	autocmd BufNewFile *.cpp :call AutoCpp()
+]])
 
 
 
--- Selecionar tudo com 'Ctrl + A'
-vim.cmd([[ map <C-a> ggVG ]])
-
--- Desfazer tudo com 'Ctrl + Z'
-vim.cmd([[ nnoremap <C-z> :u<CR> ]])
-vim.cmd([[ inoremap <C-z> <Esc>:u<CR>l ]])
-vim.cmd([[ vnoremap <C-z> <Esc>:u<CR> ]])
-
-
--- BASH - Auto preenche arquivos .sh que não existirem com a SheBang
---vim.cmd([[ autocmd BufNewFile *.sh :call append(0, '#!/usr/bin/env bash') ]])
-
--- Se não existir o arquivo C++ de extensão .cpp ele preenche da forma abaixo
--- altere para sua(s) linguagens preferidas
---vim.cmd([[
--- 	function! AutoCpp()
--- 	  call append(0, '#include <iostream>')
--- 	  call append(1, '')
--- 	  call append(2, 'int main( int argc , char **argv ){')
--- 	  call append(3, "  std::cout << \"Olá, Mundo!\" << '\\n';")
--- 	  call append(4, '  return 0;')
--- 	  call append(5, '}')
--- 	  call cursor(4, 17)
--- 	endfunction
--- 	autocmd BufNewFile *.cpp :call AutoCpp()
--- ]])
+--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--
+--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--
+-- Referencias :
+-- https://terminalroot.com.br/2021/11/instalando-plugins-tudo-sobre-neovim-com-lua.html
+-- O basico em portugues https://guialinux.uniriotec.br/vim/
+-- Super completo https://devhints.io/vim 
+--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--
+--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--::--
